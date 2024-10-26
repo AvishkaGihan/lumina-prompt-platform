5cd75a2f3ae0e0f5dd638caa5d8e12a59fe8801a
Avishka Gihan <87961869+AvishkaGihan@users.noreply.github.com>
2024-10-26 16:15:05 +0530

    data models added

--- utils/database.js
+++ utils/database.js
@@ -2,20 +2,6 @@ import mongoose from "mongoose";
 
 let isConnected = false;
 
-/**
- * Connects to the MongoDB database using Mongoose.
- *
- * This function sets the Mongoose strictQuery option to true and checks if the
- * database is already connected. If it is not connected, it attempts to connect
- * to the MongoDB database using the connection URI specified in the environment
- * variable `MONGODB_URI`. Upon successful connection, it sets the `isConnected`
- * flag to true and logs a success message. If an error occurs during the connection
- * attempt, it logs the error.
- *
- * @async
- * @function connectToDB
- * @returns {Promise<void>} A promise that resolves when the connection attempt is complete.
- */
 export const connectToDB = async () => {
   mongoose.set("strictQuery", true);
 
